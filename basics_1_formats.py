8b8f0c87b1b583a9429632f38e01c20212e20390
Petr Moravec <moravec.pt@seznam.cz>
2020-02-08 21:47:46 +0100

    formatovani textu

---
+++ basics_1_formats.py
@@ -0,0 +1,6 @@
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, "
+      "\n\t\tLike a diamond in the sky."
+      " \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+
+# lomeno n odradkuje na novy radek
+# lomeno t, je posun o jeden tabelator
