4da5fcf03cf5bae054a5626c9cb9cbf846bcdf01
Petr Moravec <moravec.pt@seznam.cz>
2020-02-09 20:03:43 +0100

    vypsani aktualniho datumu a casu

---
+++ basics_3_current_date_and_time.py
@@ -0,0 +1,6 @@
+import datetime
+now = datetime.datetime.now()
+print("current date and time: ")
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+# kdyz se predto nenapise procento, nezobrazuje se datum/cas
