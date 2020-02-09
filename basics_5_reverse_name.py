5a6c21aae510e9944877d7e38f716b5278fe6987
Petr Moravec <moravec.pt@seznam.cz>
2020-02-09 20:20:21 +0100

    Jmeno a prijmeni v obracenem poradi

---
+++ basics_5_reverse_name.py
@@ -0,0 +1,5 @@
+FName = str (input("vlozte prvni jmeno: "))
+LName = str (input("vlozte druhe jmeno: "))
+print("jmeno a prijmeni je: " + LName + " " + FName)
+
+# cist jmeno lze i bez definice typu jako napr Name = input("napiste nejake jmeno")
