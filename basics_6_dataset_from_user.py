4847e5ea922bf392b857e2968ca5cb17da2ddcc7
Petr Moravec <moravec.pt@seznam.cz>
2020-02-09 20:29:02 +0100

    vypsani sady cisel

---
+++ basics_6_dataset_from_user.py
@@ -0,0 +1,7 @@
+sada1 = input("Uzivateli, napis nejaka cisla oddelena carkou")
+list = sada1.split(",")
+tuple = tuple(list)
+print('List : ',list)
+print('Tuple : ',tuple)
+
+#list je ve hranatych zavorkach, tuple v kulatych
