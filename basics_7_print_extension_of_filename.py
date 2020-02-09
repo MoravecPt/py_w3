54d99c6904510e13755481bec8cebb275290e3b0
Petr Moravec <moravec.pt@seznam.cz>
2020-02-09 20:50:10 +0100

    zjisteni pripony souboru

---
+++ basics_7_print_extension_of_filename.py
@@ -0,0 +1,5 @@
+nazev_souboru = input("napiste cele jmeno souboru: ")
+pripona = nazev_souboru.split(".")
+print ("Pripona souboru je : " + (pripona[-1]))
+
+#v hranate zavorce urcuje, kolikate poradi za priponou (za teckou) mam zobrazit... -1 je pocet zprava od konce
