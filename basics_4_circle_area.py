9db21bb24094abf4ce1e82d55b1dfeb1f123e772
Petr Moravec <moravec.pt@seznam.cz>
2020-02-09 20:12:41 +0100

    radius of the circle

---
+++ basics_4_circle_area.py
@@ -0,0 +1,6 @@
+from math import pi
+r = float (input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
+# když slučuju delši text s retezcem musim to sloucit pres str
+# neco **2 je neco na druhou
