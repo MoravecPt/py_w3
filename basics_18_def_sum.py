9bdf7867bfadeb4a910affc12b73f8a1763029fa
Petr Moravec <moravec.pt@seznam.cz>
2020-02-25 16:03:20 +0100

    definovani sumy cisel

---
+++ basics_18_def_sum.py
@@ -0,0 +1,13 @@
+def suma (a,b,c):
+
+    sum = (a+b+c)
+
+    if a==b==c:
+        sum = sum * 3
+
+    return sum
+
+print(suma(1,2,3))
+print(suma(3,3,3))
+
+#provede soucet tri zadanych cisel, kdyz jsou shodne, tak je vynasobi trema
