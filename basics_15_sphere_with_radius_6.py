d86531dcf492e68dc139d2666f4fe03e246765ad
Petr Moravec <moravec.pt@seznam.cz>
2020-02-25 15:32:51 +0100

    vypocet objemu koule

---
+++ basics_15_sphere_with_radius_6.py
@@ -0,0 +1,9 @@
+#objem koule se spocita jako V = 4/3 * Pi * r**3 = Pi * d3/6
+
+import math
+pi = math.pi
+r = 6.0
+V = 4.0/3.0 * pi * r**3
+print ("objem koule s radiusem 6 je :", V)
+
+#mocnina se provede pomoci 2x **
