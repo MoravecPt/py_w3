3f30aaae62b3964c19062e4c11a06c69c57c2e3c
Petr Moravec <moravec.pt@seznam.cz>
2020-02-25 15:42:23 +0100

    rozdil cisel a definovani funkce

---
+++ basics_16_number_differences.py
@@ -0,0 +1,11 @@
+def difference (n):
+    if n <= 17:
+        return 17 - n
+    else:
+        return (n - 17) * 2
+
+print(difference(22))
+print(difference(14))
+
+#pomoci def se v pythonu definuje funkce
+#primo pod def nemuze byt hnedkon print, ale musi tam byt nejaka logika, kterou pak muzu kdykoliv pozdeji vyvolat zavolanim jejiho nazvu
