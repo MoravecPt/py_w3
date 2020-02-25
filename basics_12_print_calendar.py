6e04fdec69f7cbeca5c32d0843c6a479ba9d77ec
Petr Moravec <moravec.pt@seznam.cz>
2020-02-25 14:54:56 +0100

    tisk kalendare

---
+++ basics_12_print_calendar.py
@@ -0,0 +1,6 @@
+import calendar
+y = int(input("Zadejte rok :"))
+m = int(input("Zadejte mesic :"))
+print(calendar.month(y,m))
+
+#vytisknuti jednoducheho kalendare, nejprve si ho naimportuju, pak pozaduju ciselny vstup roku a mesice a nakonec to vytisknu
