75daf8218be43bac82bd6332e393b05f4c03feff
Petr Moravec <moravec.pt@seznam.cz>
2020-03-11 19:57:22 +0100

    rozdelovani cisel na suda a licha

---
+++ basics_21_even_odd_numbers.py
@@ -0,0 +1,9 @@
+cislo = int(input("uzivateli zadej nejake cislo:"))
+mod = cislo % 2
+
+if mod > 0:
+    print("cislo je liche")
+else:
+    print("cislo je sude")
+
+#pozor! deleni se pise jako procento :-) = %
