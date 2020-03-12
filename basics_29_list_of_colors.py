27a21f3e93fe22aa34690b099574e6103a29ddf2
Petr Moravec <moravec.pt@seznam.cz>
2020-03-12 20:10:35 +0100

    hledani rozdilu mezi poli

---
+++ basics_29_list_of_colors.py
@@ -0,0 +1,6 @@
+color_list_1 = set(["White","Black","Red"])
+color_list_2 = set(["Red","Green"])
+
+print(color_list_1.difference(color_list_2))
+
+# zobrazeni barev, co jsou pritomne v listu 1, ale nejsou v listu 2
