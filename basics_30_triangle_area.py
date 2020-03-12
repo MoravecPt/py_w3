ce6d582164810810d5c5516aeba58446fbff7bd3
Petr Moravec <moravec.pt@seznam.cz>
2020-03-12 20:15:39 +0100

    oblast trojuhelnika

---
+++ basics_30_triangle_area.py
@@ -0,0 +1,6 @@
+base = int(input('zadejte delku zakladny trouhelnika :'))
+height = int(input('zadejte vysku od zakladny trojuhelnika :'))
+
+area = base*height/2
+
+print("oblast trouhelnika je: ", area)
