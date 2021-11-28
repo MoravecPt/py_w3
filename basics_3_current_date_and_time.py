0b25441f411864b0bedd95ffe051be46898d6986
Peta <moravec.pt@gmail.com>
2021-11-28 14:01:44 +0100

    test commit

--- basics_3_current_date_and_time.py
+++ basics_3_current_date_and_time.py
@@ -3,6 +3,5 @@ now = datetime.datetime.now()
 print("current date and time: ")
 print(now.strftime("%Y-%m-%d %H:%M:%S"))
 
-print (test commit)
-
+print("test")
 # kdyz se predto nenapise procento, nezobrazuje se datum/cas
